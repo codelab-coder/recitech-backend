5749bce276411ef4eb12324eff6a06a3d550d572
Fernando Marques Guimarães <nandaomguimaraes@gmail.com>
2025-11-21 03:33:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -23,11 +23,12 @@ app.use(cors());
 app.use(helmet());
 app.use(compression());
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true })); // OBRIGATÓRIO pro upload do Expo
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(rateLimit({
-  windowMs: 60_limit: 100,
-  message: { success: false, error: "Muitas requisições, calma aí!" }
+  windowMs: 60 * 1000,   // 60 segundos
+  max: 100,
+  message: { success: false, error: "Muitas requisições, aguarde um minuto." }
 }));
 
 // MongoDB
